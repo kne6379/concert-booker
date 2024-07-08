79e8228468115a6109a16ffe958b878179e258c4
Robbie <Robbie@email.com>
2024-07-08 11:27:37 +0900

    fix:전체 예매 좌석 정보 수정

--- src/seat/seats.service.ts
+++ src/seat/seats.service.ts
@@ -1,13 +1,10 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import _ from 'lodash';
-import { title } from 'process';
 import { Repository } from 'typeorm';
 import { SalesSeat } from './entities/sales-seat.entity';
 import { SeatGrade } from './entities/seat-grade.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Ticket } from 'src/ticket/entities/ticket.entity';
-import Redis from 'ioredis';
-import { InjectRedis } from '@liaoliaots/nestjs-redis';
 import { Show } from 'src/show/entities/show.entity';
 import { Showdate } from 'src/show/entities/showdate.entity';
 
@@ -24,8 +21,6 @@ export class SeatsService {
     private readonly showRepository: Repository<Show>,
     @InjectRepository(Showdate)
     private readonly showDateRepository: Repository<Showdate>,
-    @InjectRedis()
-    private readonly redis: Redis,
   ) {}
 
   async availableSeat(showDate: Date, title: string) {
@@ -47,63 +42,38 @@ export class SeatsService {
       where: { showdateId: showDateData.id },
     });
 
-    // redis에 저장된 좌석 정보 가져오기
-    const redisData = await this.redis.get(`title:${title}`);
+    const remainingSeats = {};
 
-    let remainingSeats = {};
-
-    // 좌석 정보가 없을 때 실행
-    if (_.isNil(redisData)) {
-      // 좌석 등급 : 좌석 번호 객체 생성
-      const createSeatNumber = (start: number, end: number): number[] => {
-        const seats: number[] = [];
-        for (let i = start; i <= end; i++) {
-          seats.push(i);
-        }
-        return seats;
-      };
-      const availableSeats = {};
-
-      seatGradesData.forEach((grade) => {
-        availableSeats[grade.seatGrades] = createSeatNumber(
-          1,
-          grade.gradeSeatCount,
-        );
-      });
-
-      // 판매된 좌석 번호
-      const soldSeats = salesSeatData.map((seat) => seat.seatNumber);
-
-      for (const grade in availableSeats) {
-        remainingSeats[grade] = availableSeats[grade].filter(
-          (seat) => !soldSeats.includes(seat),
-        );
+    // 좌석 등급 : 좌석 번호 객체 생성
+    const createSeatNumber = (start: number, end: number): number[] => {
+      const seats: number[] = [];
+      for (let i = start; i <= end; i++) {
+        seats.push(i);
       }
-      await this.redis.set(`title:${title}`, JSON.stringify(remainingSeats));
-    } else {
-      remainingSeats = JSON.parse(redisData);
-      const latestSoldSeats = salesSeatData.map((seat) => seat.seatNumber);
+      return seats;
+    };
+    const availableSeats = {};
 
-      for (const grade in remainingSeats) {
-        remainingSeats[grade] = remainingSeats[grade].filter(
-          (seat) => !latestSoldSeats.includes(seat),
-        );
-        await this.redis.set(`title:${title}`, JSON.stringify(remainingSeats));
-      }
-      const findsalesSeat = await this.redis.get(`title:${title}`);
-      return findsalesSeat;
+    seatGradesData.forEach((grade) => {
+      availableSeats[grade.seatGrades] = createSeatNumber(
+        1,
+        grade.gradeSeatCount,
+      );
+    });
+
+    // 판매된 좌석 번호
+    const soldSeats = salesSeatData.map((seat) => seat.seatNumber);
+
+    for (const grade in availableSeats) {
+      remainingSeats[grade] = availableSeats[grade].filter(
+        (seat) => !soldSeats.includes(seat),
+      );
     }
+    return remainingSeats;
   }
-  async createSeatNumber(start: number, end: number) {}
 }
 
 // 등급 별로 해당하는 좌석 개수를 갖고 있는 객체
 // [1, 2, 3, 4, ... 20 ]
 // 등급 별로 해당하는 좌석의 개수가 다르므로 등급에 맞는 개수 책정
 // 존재하는 티켓의 번호를 제외한 번호가 들어가 있어야한다.
-
-// if (!remainingSeats[seatGrades].includes(seatNumber)) {
-//   throw new BadRequestException(
-//     `해당 좌석 번호가 ${seatGrades} 등급에 유효하지 않습니다.`,
-//   );
-// }
